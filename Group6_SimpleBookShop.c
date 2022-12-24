4e633dbd9db6cc31bef7da40d9faf93f6d00f19a
Md. Shakhiul Abrar <114344002+AbrarShakhi@users.noreply.github.com>
2022-12-24 22:25:56 +0600

    Comments

--- Group6_SimpleBookShop.c
+++ Group6_SimpleBookShop.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
-#include<conio.h>
-#include<string.h> 
-#include<stdlib.h> 
+#include<stdio.h> //Standard input & output header file.
+#include<conio.h> //Console input $ output header file. [We have used getch() functiion.]
+#include<string.h> //String header file. [We jave used strcmp() function]
+#include<stdlib.h> //Standard library header file. [We have used flush() and system("cls") function]
 
-int SerialNumber;
+int SerialNumber; //Declare a integer veriable that can be used by all the fuction.
 struct book
 {
     char bName[50];
@@ -11,62 +11,70 @@ struct book
     float bPrice;
     int bCopies;
 };
-struct book lib[500];
+struct book lib[500]; //Declare struct arrary vaeeriable that can be used by all the fuction.
 
+//Function prototype.
 void ShowWelcomeMessage();
 void ShowGoodByeMessage();
 void ShowMenuOption();
 void ShowBooks();
 void AddBook();
-int SearchBook();
+void SearchBook();
 void ShowNumberOfBooks();
 
+//main fuction.
 int main()
 {
-    SerialNumber=0;
-    int navigator;
+    SerialNumber=0; /*First SerialNumber = 0
+    This number will increase if user add a book.*/
 
-    ShowWelcomeMessage();
-    ShowMenuOption();
-    again :
+    ShowWelcomeMessage(); //Function call .It will show the welcome message.
+    ShowMenuOption(); //It will show the Menu option.
+    
+    int navigator; //integer variable for switch case. 
+    again: //Tsis is for goto statement.
     printf("\n");
     printf("Enter your choice : ");
-    scanf("%d",&navigator);
+    scanf("%d",&navigator); //It will take a integer and store it to navigator. 
     printf("\n");
-    switch(navigator)
+    fflush(stdin); /*Without fflush function, If the user input a carecter or string in [scanf("%d",&navigator);-line 38] It will make a infinite loop.
+    The purpose of fflush(stdin) is to clean (or flush) the output buffer and transfer the buffered data into Terminal or Disk.*/
+
+    switch(navigator) //if the navigator is 1 to 6 then it will do certain tasks.
     {
         case 1:
-            ShowMenuOption();
+            ShowMenuOption(); //If the user enter 1 then call ShowMenuOption() and show the Menu option.
         break;
         case 2:
-            ShowBooks();
+            ShowBooks(); //If the user enter 2 then call ShowBooks() function and show all the added books.
         break;
         case 3:
-            AddBook();
+            AddBook(); //If the user enter 3 then call AddBook() function.
         break;
         case 4:
-            SearchBook();
+            SearchBook(); //If the user enter 4 then call SearchBook() function.
         break;
         case 5:
-            ShowNumberOfBooks();
+            ShowNumberOfBooks(); //If the user enter 5 then call ShowNumberOfBooks() function.
         break;
         case 6:
-            system("cls");
-            ShowGoodByeMessage();
-            return 0;
+            system("cls"); //It will clear the console screen before Goodbye message.
+            ShowGoodByeMessage(); //If the user enter 6 then call ShowGoodByeMessage() function.
+            getch(); //Program will wait before ending for user to enter any key
+            return 0; //and program will end right here.
         break;
-        default:
-            printf("\nInvalid Inout.\n");
-            ShowMenuOption();
-            goto again;
+        default: //If useer enter invalid input (navigator<1 || bavigaror>6)
+            printf("\nInvalid Inout.\n"); //Show a Invalid message.
+            ShowMenuOption(); //call ShowMenuOption()
+            goto again; //jump statement
         break;
     }
-    goto again;
+    goto again; //It will jump to again.
 }
-
+//Function Defination.
 void ShowWelcomeMessage()
 {
-    system("cls");
+    //Welcome message
     printf("*****************************************\n");
     printf("\n");
     printf("Welcome to Simple Book Management System.\n");
@@ -77,15 +85,16 @@ void ShowWelcomeMessage()
 }
 void ShowGoodByeMessage()
 {
+    //Goodbye message
     printf("**************************************************\n");
     printf("\n");
     printf("Thank you for using Simple Book Management System\n");
     printf("\n");
     printf("**************************************************\n");
-    getch();
 }
 void ShowMenuOption()
 {
+    //Menu option
     printf("Please enter a menu option number:\n");
     printf("1) Display this menu.\n");
     printf("2) Show all available books.\n");
@@ -95,15 +104,15 @@ void ShowMenuOption()
     printf("6) Exit from this book shop.\n");
     printf("\n");
 }
-void ShowBooks()
+void ShowBooks() //Defination of ShowBooks() function.
 {
-    if (SerialNumber==0)
+    if (SerialNumber==0) //If user do not add any books then the condition is true and print this message.
         printf("Opps! Looks like shelfs are empty.\n");
-    else
+    else //If user add any books then SerialNumber!=0.
     {
-        int i;
         printf("List of books:\n\n");
-        for (i=0; i<SerialNumber; i++)
+        int i; //Declare a veriable for loop.
+        for (i=0; i<SerialNumber; i++) //loop will run until i<SerialNumber.
         {
             printf("Book title: %s", lib[i].bName);
             printf("Book author: %s", lib[i].bAuthor);
@@ -114,7 +123,6 @@ void ShowBooks()
 }
 void AddBook()
 {
-    fflush(stdin);
     printf("Enter book title: ");
     fgets(lib[SerialNumber].bName, 50, stdin);
     
@@ -123,36 +131,36 @@ void AddBook()
 
     printf("Enter book price: ");
     scanf("%f", &lib[SerialNumber].bPrice);
+    fflush(stdin);
 
     printf("Enter number of copies: ");
     scanf("%d", &lib[SerialNumber].bCopies);
-
-    ++SerialNumber;
-}
-int SearchBook()
-{
-    char searchtitle[50];
     fflush(stdin);
+
+    ++SerialNumber; //SerialNumber will increase if user adds book.
+}
+void SearchBook()
+{
     printf("Enter book title to search: ");
+    char searchtitle[50]; //Declare a veriable to store a string
     fgets(searchtitle, 50, stdin);
     printf("\n");
     int i, x=0;
     for (i=0; i<SerialNumber; i++)
     {
-        if (strcmp(searchtitle, lib[i].bName)==0)
+        if (strcmp(searchtitle, lib[i].bName)==0) //Compare two string
         {
             printf("Book title: %s", lib[i].bName);
             printf("Book author: %s", lib[i].bAuthor);
             printf("Book price: %.2f\n", lib[i].bPrice);
             printf("Copies: %d\n\n", lib[i].bCopies);
-            x++;
         }
+        x++; //x will increase if book matches
     }
-    if(x==0) printf("Book not found.\n");
-
-    return 0;
+    if(x==0) printf("Book not found.\n"); 
+    //If Search dont match then it will print this message.
 }
 void ShowNumberOfBooks()
-{
+{//total number of books is SerialNumber.
     printf("Total number of books in the shop: %d\n", SerialNumber);
 }
